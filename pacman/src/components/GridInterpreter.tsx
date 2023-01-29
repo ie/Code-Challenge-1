e825074cd4a1414adb701c53df7a967a897dd793
Kito <kito@192-168-1-101.tpgi.com.au>
2023-01-29 20:40:54 +1100

    placePac, retrieveCommands util functions

--- pacman/src/components/GridInterpreter.tsx
+++ pacman/src/components/GridInterpreter.tsx
@@ -1,20 +1,17 @@
 import React, { useState, useCallback } from 'react'
 import Loader from './Loader'
 
-interface IPacPosition {
-    xPos: number;
-    yPos: number;
-    direction: number;
-}
+//helpers
+import { retrieveCommands, executeCommands } from '../util/helpers'
 
 const GridInterpreter = (): JSX.Element => {
-    const [pacPosition, setPacPosition] = useState<IPacPosition>({xPos: 0, yPos: 0, direction: 0})
-    const [hasPacMoved, setHasPacMoved] = useState<boolean>(false)
     const [loading, setLoading] = useState<boolean>(false)
 
-    const interpret = useCallback((): void => {
+    const interpret = useCallback(() => {
         setLoading(true)
-        //readline util function
+        retrieveCommands().then((commands) => {
+            // executeCommands(commands)
+        }).then(() => setLoading(false))
 
     }, [])
 
---
+++ pacman/src/util/helpers.tsx
@@ -0,0 +1,103 @@
+/**
+  * fetches commands txt file, splits it by line and returns an array of commands
+  * @return {any} Array of commands
+  */
+export const retrieveCommands = async (): Promise<string[][]> => {
+    return fetch('commands.txt')
+    .then((r) => r.text())
+    .then(text  => {
+      const commands:string[][] = text.split('\n').filter((line) => line.length).map((line) => line.split(' '))
+      return commands
+    })
+}
+
+interface IPacPosition {
+  xPos: number;
+  yPos: number;
+  direction: number;
+}
+
+enum COMMAND_TYPES {
+  PLACE = 'place',
+  MOVE = 'move',
+  LEFT = 'left',
+  RIGHT = 'right', 
+  REPORT = 'report'
+}
+
+/**
+  * breaks down and executes commands line by line
+  * @param {string[][]} commands The array of string array commands
+  */
+export const executeCommands = (commands: string[][]) => {
+  let pacPosition:IPacPosition | undefined
+  let hasPacMoved = false
+
+  for (const command of commands) {
+    if (!hasPacMoved && command[0] !== COMMAND_TYPES.PLACE) {
+      continue;
+    }
+    switch(command[0].toLowerCase()) {
+      case COMMAND_TYPES.PLACE:
+          //place util
+          if (command.length > 1) {
+            const placePosition = command[1].split(',')
+            if (placePosition.length === 3) {
+              pacPosition = placePac(placePosition)
+              if (pacPosition) {
+                hasPacMoved = true
+              }
+            }
+          }
+        break;
+      case COMMAND_TYPES.MOVE:
+        //move util
+        break;
+      case COMMAND_TYPES.LEFT || COMMAND_TYPES.RIGHT:
+        //rotate
+        break;
+      case COMMAND_TYPES.REPORT:
+        alert('NORTH 23 2 1')
+    }
+  }
+}
+
+const DirectionDegrees: Record<string, number> = {
+  "NORTH": 0,
+  "EAST": 90,
+  "SOUTH": 180,
+  "WEST": 270
+}
+
+/**
+  * breaks down and executes commands line by line
+  * @param {string[][]} commands The array of string array commands
+  */
+const placePac = (command: string[]): IPacPosition | undefined => {
+  let isValidPos = true
+
+  const convertStrToNum = (pos: string): number | undefined => {
+    if (/^\d+$/.test(pos)) {
+      const num = Number(pos)
+      if (Number.isInteger(num) && num >= 0 && num <= 5) {
+        return num
+      }
+    } 
+    isValidPos = false
+    return undefined
+  }
+
+  const convertDirToDegree = (direction: string): number | undefined => {
+    if (['NORTH', 'SOUTH', 'EAST', 'WEST'].includes(command[2])) {
+      return DirectionDegrees[command[2]]
+    }
+    isValidPos = false
+    return undefined
+  }
+
+  const x = convertStrToNum(command[0])
+  const y = convertStrToNum(command[1])
+  const direction = convertDirToDegree(command[2])
+
+  return isValidPos ? {xPos: x, yPos: y, direction: direction} as IPacPosition : undefined
+}
