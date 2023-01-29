f5bff8d5ef854dd30d43864c644dc6f2c12ae0e2
Kito <kito@192-168-1-101.tpgi.com.au>
2023-01-29 19:13:48 +1100

    App.tsx cleanup

--- pacman/src/App.tsx
+++ pacman/src/App.tsx
@@ -6,17 +6,6 @@ const App = (): JSX.Element => {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <GridInterpreter />
       </header>
     </div>
